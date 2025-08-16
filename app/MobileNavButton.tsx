5162375150836d8dcded0ac34186169a28f146c1
Osman Yakub Beni <osmanbeni2001@gmail.com>
2025-08-16 13:12:47 +0100

    fix: Mobile book link

--- app/MobileNavButton.tsx
+++ app/MobileNavButton.tsx
@@ -13,7 +13,7 @@ const MobileNavButton = () => {
       </DropdownMenu.Trigger>
       <DropdownMenu.Content>
         <DropdownMenu.Item asChild>
-          <Link href={"/books"}>Book List</Link>
+          <Link href={"/books?page=1"}>Book List</Link>
         </DropdownMenu.Item>
         <DropdownMenu.Item asChild>
           <Link href={"/about"}>About us</Link>
